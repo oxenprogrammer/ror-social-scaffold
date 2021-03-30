c841f268a1c62f248a9445bef45b96147bc52b91
oxenprogrammer <emmy1000okello@gmail.com>
2021-03-30 21:05:50 +0300

    feat(model): update status type to integer

--- app/models/friendship.rb
+++ app/models/friendship.rb
@@ -1,4 +1,5 @@
 class Friendship < ApplicationRecord
   belongs_to :requestor, class_name: 'User', foreign_key: 'requestor_id'
   belongs_to :requestee, class_name: 'User', foreign_key: 'requestee_id'
+  enum status: %i[pending accepted rejected]
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_30_172210) do
+ActiveRecord::Schema.define(version: 2021_03_30_175430) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,7 +28,7 @@ ActiveRecord::Schema.define(version: 2021_03_30_172210) do
   create_table "friendships", force: :cascade do |t|
     t.integer "requestor_id"
     t.integer "requestee_id"
-    t.string "status"
+    t.integer "status", default: 0
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
