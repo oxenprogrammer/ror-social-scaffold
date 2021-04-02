17a0ab172fc1b0f04d5ad27f218de5ec56a2a5c4
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-02 07:18:59 +0300

    text(controller): update rails_helper

--- spec/factories.rb
+++ spec/factories.rb
@@ -6,6 +6,13 @@ FactoryGirl.define do
     password_confirmation '123456'
   end
 
+  factory :user1 do
+    name 'emmy'
+    email 'emmy@example.com'
+    password '123456'
+    password_confirmation '123456'
+  end
+
   factory parent: :user do
     after(:create)
   end
