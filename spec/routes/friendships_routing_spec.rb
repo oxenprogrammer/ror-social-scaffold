5161bc4ae49d79c2ca87d12fc273309e8ec67bb3
oxenprogrammer <emmy1000okello@gmail.com>
2021-04-04 15:13:49 +0300

    Hotfix(rubocop): fix rubocop warning

--- spec/routes/friendships_routing_spec.rb
+++ spec/routes/friendships_routing_spec.rb
@@ -6,7 +6,6 @@ RSpec.describe FriendshipsController, type: :routing do
       expect(post: '/users/1/friendships').to route_to('friendships#create', user_id: '1')
     end
 
-
     it 'routes to #update' do
       expect(patch: '/users/1/friendships/1').to route_to('friendships#update', user_id: '1', id: '1')
     end
